595dc9092ea030b968947c44e8cc7e5a19630cdc
Yuval Ron <yuvalron@hotmail.com>
2018-11-07 23:26:05 +0200

    Added unit test for Int

--- src/main/java/il/org/spartan/utils/Int.java
+++ src/main/java/il/org/spartan/utils/Int.java
@@ -1,5 +1,9 @@
 package il.org.spartan.utils;
 
+import static fluent.ly.azzert.*;
+
+import org.junit.*;
+
 import fluent.ly.*;
 
 /** A poor man's approximation of a mutable int, which is so much more
@@ -61,4 +65,67 @@ public final class Int {
   public void clear() {
     inner = 0;
   }
+  
+  @SuppressWarnings("static-method") public static class TEST {
+    @Test public void testGet() {
+      Int i = new Int(3);
+      azzert.that(i.get(), is(3));
+    }
+    
+    @Test public void testStep() {
+      Int i = new Int(3);
+      i.step();
+      azzert.that(i.get(), is(4));
+      i.step();
+      azzert.that(i.get(), is(5));
+    }
+    
+    @Test public void testAdd() {
+      Int i = new Int(3);
+      i.add(2);
+      azzert.that(i.get(), is(5));
+      i.add(0);
+      azzert.that(i.get(), is(5));
+      i.add(-6);
+      azzert.that(i.get(), is(-1));
+    }
+    
+    @Test public void testSet() {
+      Int i = new Int(3);
+      i.set(2);
+      azzert.that(i.get(), is(2));
+      i.set(-1);
+      azzert.that(i.get(), is(-1));
+      i.set(0);
+      azzert.that(i.get(), is(0));
+    }
+    
+    @Test public void testNext() {
+      Int i = new Int(3);
+      i.next();
+      azzert.that(i.get(), is(4));
+      i.next();
+      azzert.that(i.get(), is(5));
+    }
+    
+    @Test public void testAddOther() {
+      Int i1 = new Int(3);
+      Int i2 = new Int(4);
+      i1.add(i2);
+      azzert.that(i1.get(), is(7));
+      azzert.that(i2.get(), is(4));
+    }
+    
+    @Test public void testClear() {
+      Int i = new Int(3);
+      i.clear();
+      azzert.that(i.get(), is(0));
+    }
+    
+    
+    
+  }
+  
+  
+  
 }
