25d48cb1e72ae1876998f5b4b1a152cc732db9bf
guyme <guy.menhel@gmail.com>
2018-11-10 01:10:20 +0200

    #52 : added unit test for string

--- src/main/java/il/org/spartan/statistics/Statistics.java
+++ src/main/java/il/org/spartan/statistics/Statistics.java
@@ -56,7 +56,7 @@ public abstract class Statistics {
       $ += ¢;
       sum2 += ¢ * ¢;
     }
-    return sum2 / (ds.length) - $ * $ / (ds.length * ds.length);
+    return sum2 / ds.length - $ * $ / (ds.length * ds.length);
   }
 
   protected int n;
