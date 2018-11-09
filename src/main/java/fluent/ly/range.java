7ca500a4ba97eadda6b9eb3468a4fc0ee00a86d2
A'meer A'mer <ameeramer@Ameers-MacBook-Pro.local>
2018-11-09 17:54:33 +0200

    fixed issue #32 - unit test for class range

--- src/main/java/fluent/ly/range.java
+++ src/main/java/fluent/ly/range.java
@@ -1,7 +1,11 @@
 package fluent.ly;
 
+import static fluent.ly.azzert.*;
+
 import java.util.*;
 import java.util.stream.*;
+
+import org.junit.*;
 
 /** A fluent API implementation for range
  * @author Dor Ma'ayan
@@ -9,7 +13,7 @@ import java.util.stream.*;
 public class range {
   public class AfterTo extends RangeIterator<AfterTo> {
     public AfterTo from(final int ¢) {
-      to = ¢;
+      from = ¢;
       return this;
     }
 
@@ -118,15 +122,15 @@ public class range {
   }
 
   public static RangeIterator<?> naturals() {
-    return from(0).to(-1).step(1);
+    return from(0).to(-1).step(1).infiniteRange();
   }
 
   public static RangeIterator<?> numerals() {
-    return from(1).to(-1).step(1);
+    return from(1).to(-1).step(1).infiniteRange();
   }
 
   public static RangeIterator<?> odds() {
-    return from(1).to(-1).step(2);
+    return from(1).to(-1).step(2).infiniteRange();
   }
 
   public static <T> RangeIterator<?> of(final T[] ¢) {
@@ -164,4 +168,87 @@ public class range {
   boolean infinite;
   int step = 1;
   int to = -1;
+  
+  @SuppressWarnings("static-method") public static class TEST {
+    @Test public void firstTwoNaturals(){
+      Iterator<Integer> it = naturals().iterator();
+      azzert.that(it.next(), is(0));
+      azzert.that(it.next(), is(1));
+      
+      Iterator<Integer> it2 = to(-1).from(0).step(1).infiniteRange().iterator();
+      azzert.that(it2.next(), is(0));
+      azzert.that(it2.next(), is(1));
+      
+      Iterator<Integer> it3 = infinite().iterator();
+      azzert.that(it3.next(), is(0));
+      azzert.that(it3.next(), is(1));
+      
+      Iterator<Integer> it4 = to(-1).from(0).infinite().iterator();
+      azzert.that(it4.next(), is(0));
+      azzert.that(it4.next(), is(1));
+      
+      Iterator<Integer> it5 = from(0).self().step(1).infinite().iterator();
+      azzert.that(it5.next(), is(0));
+      azzert.that(it5.next(), is(1));
+      
+      Iterator<Integer> it6 = from(0).infinite().iterator();
+      azzert.that(it6.next(), is(0));
+      azzert.that(it6.next(), is(1));
+      
+      Iterator<Integer> it7 = infiniteFrom(0,4).from(2).iterator();
+      azzert.that(it7.next(), is(2));
+      azzert.that(it7.next(), is(3));
+    }
+    
+    @Test public void firstTwoNumerlas(){
+      Iterator<Integer> it = numerals().iterator();
+      azzert.that(it.next(), is(1));
+      azzert.that(it.next(), is(2));
+    }
+    
+    @Test public void firstTwoOdds(){
+      Iterator<Integer> it = odds().iterator();
+      azzert.that(it.next(), is(1));
+      azzert.that(it.next(), is(3));
+    }
+    
+    @Test public void rangeOfArray(){
+      Iterator<Integer> it = of(new Double[] {Double.valueOf(1.1),Double.valueOf(2.2)
+          ,Double.valueOf(3.3)}).iterator();
+      azzert.that(it.next(), is(0));
+      azzert.that(it.next(), is(1));
+      azzert.that(it.next(), is(2));
+      try {
+        it.next();
+        azzert.fail();
+      } catch(NoSuchElementException e) {
+        e.getMessage();
+        return;
+      }
+    }
+  
+    @Test public void infiniteSameValue() {
+      Iterator<Integer> it = infinite(2).iterator();
+      azzert.that(it.next(), is(2));
+      azzert.that(it.next(), is(2));
+    }
+    
+    @Test public void exclusiveRange() {
+      Iterator<Integer> it = from(0).to(2).step(1).exclusive().iterator();
+      azzert.that(it.next(), is(0));
+      azzert.that(it.next(), is(1));
+      try {
+        it.next();
+        azzert.fail();
+      } catch(NoSuchElementException e) {
+        e.getMessage();
+        return;
+      }
+    }
+  
+    @Test public void streamOfAfter() {
+      Stream<Integer> s = to(10).from(0).step(1).stream();
+      azzert.that(s.findFirst().get(), is(0));
+    }
+  }
 }
