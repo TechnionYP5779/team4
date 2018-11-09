024c01067dc0e43bd1b35e57e1c1c07653f48b77
A'meer A'mer <ameeramer@Ameers-MacBook-Pro.local>
2018-11-09 18:50:53 +0200

    fixed issue #44 - unit tests for singleton

--- src/main/java/a/singleton.java
+++ src/main/java/a/singleton.java
@@ -19,14 +19,5 @@ public interface singleton {
   static <T> T[] array(final T ¢) {
     return as.array(¢);
   }
-/*
-  @SuppressWarnings("static-method") public static class TEST {
-    
-    @Test public void intListSingelton() {
-      singleton mock = mock(singleton.class);
-      when(mock.getAsBoolean()).thenReturn(true);
-      azzert.that(ly(mock), is(true));
-    }
-  }
-  */
+
 }
---
+++ src/test/java/a/SingletonTest.java
@@ -0,0 +1,19 @@
+package a;
+
+import static fluent.ly.azzert.*;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+@SuppressWarnings("static-method") public class SingletonTest {
+  
+  @Test public void intListSingelton() {
+    azzert.that(singleton.list(new Integer(5)).size(), is(1));
+  }
+  
+  @Test public void intArraySingelton() {
+    azzert.that(singleton.array(new Object()).length, is(1));
+  }
+}  
+
