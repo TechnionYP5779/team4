e1585743615123ea776f0e682ffd32ff57524771
guyme <guy.menhel@gmail.com>
2018-11-10 00:03:34 +0200

    #46 : documented interface forget and added unit tests

---
+++ src/test/java/fluent/ly/ForgetTest.java
@@ -0,0 +1,24 @@
+package fluent.ly;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class ForgetTest {
+  
+  @Test public void forgetAll() {
+    forget.all(Integer.valueOf(1), Integer.valueOf(2), Integer.valueOf(3));
+    forget.all(Double.valueOf(1.4), Double.valueOf(2.5), Double.valueOf(3.6));
+    forget.all(String.valueOf("1.4"), String.valueOf("2.5"), String.valueOf("3.6"));
+    forget.all(Boolean.TRUE, Boolean.FALSE);
+    
+    forget.all(Integer.valueOf(1), Double.valueOf(2.5), String.valueOf("3.6"), Boolean.FALSE);
+  }
+  
+  @Test public void forgetIt() {
+    forget.it(1L);
+    forget.it(1);
+    forget.it(1.4);
+    forget.it("1.4");
+    forget.it(true);
+  }
+  
+}
