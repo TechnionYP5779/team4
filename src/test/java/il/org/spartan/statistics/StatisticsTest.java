86c3a7e545c55814a659e2717a98608ec3f3b6e0
A'meer A'mer <ameeramer@campus.technion.ac.il>
2018-11-09 23:50:31 +0200

    issue #34 - partial tests for class statistics

---
+++ src/test/java/il/org/spartan/statistics/StatisticsTest.java
@@ -0,0 +1,23 @@
+package il.org.spartan.statistics;
+
+
+import static fluent.ly.azzert.*;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+@SuppressWarnings("static-method") public class StatisticsTest {
+  @Test public void sampleMean0() {
+    azzert.that(Statistics.sampleMean(new double[] {1.0,2.0,3.0,4.0,5.0,6.0}), is(3.5));
+  }
+  
+  @Test public void sampleVariance0() {
+    double res = Statistics.sampleVariance(new double[] {1.0,2.0,3.0,4.0,5.0,6.0});
+    azzert.aye(res - (105.0/36) > -0.001 && res - (105.0/36) < 0.001);
+  }
+  
+  @Test public void mizzing() {
+    //int miss = Statistics.missing();
+  }
+}
