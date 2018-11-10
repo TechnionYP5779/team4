17dfb472d456eb51c586a4f3869dd48e3be4030a
Yuval Ron <yuvalron@hotmail.com>
2018-11-10 15:32:51 +0200

    Added UnitTest for LongestCommonSubsequnce

---
+++ src/test/java/il/org/spartan/utils/LongestCommonSubsequenceTest.java
@@ -0,0 +1,14 @@
+package il.org.spartan.utils;
+import static fluent.ly.azzert.*;
+import org.junit.*;
+import fluent.ly.*;
+
+@SuppressWarnings("static-method") public class LongestCommonSubsequenceTest {
+  @Test public void testSimilarity() {
+    azzert.that((new LongestCommonSubsequence("abc123", "abc")).similarity(), is(2.0 / 3.0));
+    azzert.that((new LongestCommonSubsequence("abc123", "abc123")).similarity(), is(1.0));
+    azzert.that((new LongestCommonSubsequence("abc123", "def")).similarity(), is(0.0));
+    azzert.that((new LongestCommonSubsequence("abc123", "")).similarity(), is(0.0));
+    azzert.that((new LongestCommonSubsequence("", "abc123")).similarity(), is(0.0));
+  }
+}
