97ba046caa58ee7ee0e4c25bceade54a49e742cf
Yuval Ron <yuvalron@hotmail.com>
2018-11-10 13:05:53 +0200

    Added test for Str class

---
+++ src/test/java/il/org/spartan/utils/StrTest.java
@@ -0,0 +1,27 @@
+package il.org.spartan.utils;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+@SuppressWarnings("static-method") public class StrTest {
+  @Test public void testStr() {
+    azzert.isNull((new Str()).inner());
+  }
+  
+  @Test public void testIsEmptyx() {
+    azzert.aye((new Str()).isEmptyx());
+    azzert.not((new Str("message")).isEmptyx());
+  }
+  
+  @Test public void testNotEmpty() {
+    azzert.not((new Str()).notEmpty());
+    azzert.aye((new Str("message")).notEmpty());
+  }
+  
+  @Test public void testSet() {
+    Str s = new Str();
+    s.set("set");
+    azzert.not((new Str()).isEmptyx());
+  }
+}
