fd4587e8fc8a09f2a217b0236c8ae1325f940f79
Yuval Ron <yuvalron@hotmail.com>
2018-11-10 15:19:48 +0200

    finished RangeTest.

--- src/test/java/il/org/spartan/utils/RangeTest.java
+++ src/test/java/il/org/spartan/utils/RangeTest.java
@@ -2,6 +2,8 @@ package il.org.spartan.utils;
 
 import static fluent.ly.azzert.*;
 
+import java.util.*;
+
 import org.junit.*;
 
 import fluent.ly.*;
@@ -13,22 +15,14 @@ import fluent.ly.*;
     azzert.not((new Range(1, 2)).equals(r));
   }
   
-  @Test public void testHashCode() {
-    azzert.that((new Range(0, 0)).hashCode(), is(0));
-  }
-  
-  @Test public void testIsEmpty() {
-    azzert.aye((new Range(0, 0)).isEmpty());
-    azzert.not((new Range(0, 1)).isEmpty());
-  }
-  
   @Test public void testIncludedIn() {
-    azzert.aye((new Range(0, 2)).includedIn(new Range(-1, 3)));
+    azzert.aye((new Range(-1, 3)).includedIn(new Range(-1, 3)));
     azzert.aye((new Range(-1, 2)).includedIn(new Range(-1, 3)));
     azzert.aye((new Range(0, 3)).includedIn(new Range(-1, 3)));
-    azzert.aye((new Range(-1, 3)).includedIn(new Range(-1, 3)));
-    
+    azzert.aye((new Range(0, 2)).includedIn(new Range(-1, 3)));    
     azzert.not((new Range(0, 2)).includedIn(new Range(0, 1)));
+    azzert.not((new Range(-1, 1)).includedIn(new Range(0, 1)));
+    azzert.not((new Range(0, 5)).includedIn(new Range(1, 4)));
     azzert.not((new Range(0, 2)).includedIn(new Range(-3, 1)));
     azzert.not((new Range(0, 2)).includedIn(new Range(0, 3)));
   }
@@ -37,22 +31,54 @@ import fluent.ly.*;
     azzert.that(new Range(0, 5), is(new Range(0, 2).merge(new Range(3, 5))));
   }
   
+  @Test public void testIsEmpty() {
+    azzert.aye((new Range(0, 0)).isEmpty());
+    azzert.not((new Range(0, 1)).isEmpty());
+  }
+  
+  @Test public void testHashCode() {
+    azzert.that((new Range(0, 0)).hashCode(), is(0));
+  }
+  
   @Test public void testOverlapping() {
-    azzert.aye(new Range(0, 5).overlapping(new Range(0, 3)));
+    azzert.aye(new Range(0, 3).overlapping(new Range(0, 3)));
+    azzert.aye(new Range(0, 4).overlapping(new Range(0, 3)));
+    azzert.aye(new Range(-1, 3).overlapping(new Range(0, 3)));
     azzert.aye(new Range(0, 2).overlapping(new Range(0, 3)));
-    azzert.aye(new Range(0, 1).overlapping(new Range(1, 3)));
-    
+    azzert.aye(new Range(0, 1).overlapping(new Range(1, 3)));   
+    azzert.not(new Range(-1, 4).overlapping(new Range(0, 3)));
     azzert.not(new Range(0, 1).overlapping(new Range(2, 3)));
     azzert.not(new Range(2, 3).overlapping(new Range(0, 1)));
   }
   
   @Test public void testToString() {
-    azzert.that((new Range(0, 5) + ""), is("[0, 5]"));
+    azzert.that(new Range(0, 5) + "", is("[0, 5]"));
   }
   
   @Test public void testFindIncludedIn() {
-    azzert.isNull((new Range(0, 1).findIncludedIn(null)));
+    azzert.isNull(new Range(0, 1).findIncludedIn(null));
   }
   
+  @Test public void testEquals() {
+    azzert.aye(new Range(0, 1).equals(new Range(0, 1)));
+    azzert.not(new Range(0, 1).equals(null));
+    azzert.not(new Range(0, 1).equals(new Range(2, 3)));
+    azzert.not(new Range(2, 3).equals(new Range(0, 1)));
+    azzert.not(new Range(1, 2).equals(new Range(1, 3)));
+    azzert.not(new Range(1, 3).equals(new Range(1, 2)));
+    azzert.not(new Range(1, 3).equals(new Range(2, 3)));
+    azzert.not(new Range(2, 3).equals(new Range(1, 3)));
+  }
+  
+  @Test public void testPruneIncluders() {
+    LinkedList<Range> list = new LinkedList<>();
+    list.add(new Range(0, 1));
+    list.add(new Range(1, 2));
+    list.add(new Range(2, 3));
+    list.add(new Range(3, 4));
+    azzert.that(list.size(), is(4));
+    new Range(-1, -1).pruneIncluders(list);
+    azzert.that(list.size(), is(4));
+  }
   
 }
