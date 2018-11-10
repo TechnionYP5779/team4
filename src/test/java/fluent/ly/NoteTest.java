8dc6a9b126e17c9658a04b1337dc8988386e105a
Yuval Ron <yuvalron@hotmail.com>
2018-11-10 12:41:42 +0200

    Added NoteTest class

---
+++ src/test/java/fluent/ly/NoteTest.java
@@ -0,0 +1,18 @@
+package fluent.ly;
+import org.junit.*;
+import java.util.logging.*;
+
+@SuppressWarnings("static-method") public class NoteTest {
+  @Test public void testLevels() {
+    note.set(Level.SEVERE);
+    note.unset();
+    note.set(Level.WARNING);
+    note.set(Level.WARNING);
+    note.unset();
+    note.unset();
+  }
+  
+  @Test public void testInfo() {
+    azzert.isNull(note.info("message"));
+  }
+}
