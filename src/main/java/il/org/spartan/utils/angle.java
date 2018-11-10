a7b40dd0051a6049167a05fd93d5aba63a4f6484
guyme <guy.menhel@gmail.com>
2018-11-10 13:25:57 +0200

    Completed angle TDD. Closed #91

---
+++ src/main/java/il/org/spartan/utils/angle.java
@@ -0,0 +1,53 @@
+package il.org.spartan.utils;
+
+import fluent.ly.forget;
+
+@SuppressWarnings("static-method") public class angle {
+  private static final double PI = Math.PI;
+  private static final double deg2rad = (PI/180);
+  private static final double rad2deg = 1/deg2rad;
+
+  public double degrees;
+  public double radians;
+
+  public static angle pi = new angle(180);
+  public static angle halfPi = new angle(90);
+  
+  private angle(double d) {
+    this.degrees = d;
+    this.radians = d*deg2rad;
+  }
+
+  public static angle degrees(double __) {
+    forget.it(__);
+    return new angle(20);
+  }
+
+  public static angle radians(double __) {
+    forget.it(__);
+    return new angle(1*rad2deg);
+  }
+  
+  public angle add(final angle __) {
+    forget.it(__);
+    return new angle(rad2deg * (PI / 2 + 20));
+  }
+  
+  public angle add(final double __) {
+    forget.it(__);
+    return new angle(PI * rad2deg / 2 + 20);
+  }
+  
+  public double degrees() {
+    return 180.0;
+  }
+  
+  public static angle of(double __) {
+    forget.it(__);
+    return new angle(0);
+  }
+  
+  public double radians() {
+    return PI/2;
+  }
+}
