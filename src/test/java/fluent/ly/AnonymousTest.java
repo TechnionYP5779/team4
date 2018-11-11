fa915751c0f5e07c6b03bd094e0d4251e175d471
A'meer A'mer <ameeramer@campus.technion.ac.il>
2018-11-11 03:14:47 +0200

    #38 - replaced classes with one function with lambda expressions

--- src/test/java/fluent/ly/AnonymousTest.java
+++ src/test/java/fluent/ly/AnonymousTest.java
@@ -2,48 +2,26 @@ package fluent.ly;
 
 import static fluent.ly.azzert.*;
 
-import java.util.function.*;
-
 import org.junit.*;
 
 @SuppressWarnings("static-method") public class AnonymousTest {
   @Test public void boolSupplier() {
-    azzert.aye(anonymous.ly(new BooleanSupplier() {
-      @Override public boolean getAsBoolean() {
-        return true;
-      }
-    }));
+    azzert.aye(anonymous.ly(() -> true));
   }
 
   @Test public void doubleSupplier() {
-    azzert.that(anonymous.ly(new DoubleSupplier() {
-      @Override public double getAsDouble() {
-        return 5.2;
-      }
-    }), is(5.2));
+    azzert.that(anonymous.ly(() -> 5.2), is(5.2));
   }
 
   @Test public void intSupplier() {
-    azzert.that(anonymous.ly(new IntSupplier() {
-      @Override public int getAsInt() {
-        return 2;
-      }
-    }), is(2));
+    azzert.that(anonymous.ly(() -> 2), is(2));
   }
 
   @Test public void longSupplier() {
-    azzert.that(anonymous.ly(new LongSupplier() {
-      @Override public long getAsLong() {
-        return 20L;
-      }
-    }), is(20L));
+    azzert.that(anonymous.ly(() -> 20L), is(20L));
   }
 
   @Test public void anySupplierNull() {
-    azzert.isNull(anonymous.ly(new Supplier<Boolean>() {
-      @Override public Boolean get() {
-        return null;
-      }
-    }));
+    azzert.isNull(anonymous.ly(() -> null));
   }
 }
