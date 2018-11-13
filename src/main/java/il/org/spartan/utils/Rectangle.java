f43407deca631f134375995c029b2d156c80f274
max <maxcalderon@campus.technion.ac.il>
2018-11-13 17:37:10 +0200

    #75: added tests for getWidth

--- src/main/java/il/org/spartan/utils/Rectangle.java
+++ src/main/java/il/org/spartan/utils/Rectangle.java
@@ -1,14 +1,20 @@
 package il.org.spartan.utils;
 
 public class Rectangle {
-  private double y1, y2;
+  private double y1, y2, x1, x2;
   
   public Rectangle(double x1, double y1, double x2, double y2) {
     this.y1 = y1;
     this.y2 = y2;
+    this.x1 = x1;
+    this.x2 = x2;
   }
 
   public double getLen() {
-    return (this.y2 > this.y1) ? (this.y2 - this.y1) : (this.y1 - this.y2) ;
+    return (y2 > y1) ? (y2 - y1) : (y1 - y2) ;
+  }
+
+  public double getWidth() {
+    return (x2 > x1) ? (x2 - x1) : (x1 - x2) ;
   }
 }
--- src/test/java/il/org/spartan/utils/RectangleTest.java
+++ src/test/java/il/org/spartan/utils/RectangleTest.java
@@ -1,22 +1,37 @@
 package il.org.spartan.utils;
 
-import static fluent.ly.azzert.*;
-
 import org.junit.*;
 
 import fluent.ly.*;
 
 @SuppressWarnings("static-method") public class RectangleTest {
+  private final double delta = 0.0000001;
+  
   @Test public void constructor() {
     azzert.notNull(new Rectangle(10,20,30,40));
   }
   @Test public void getLen20() {
-    azzert.that((new Rectangle(10, 20, 50, 40)).getLen(), is(20.0));
+    Rectangle rec = new Rectangle(10, 20, 50, 40);
+    azzert.aye(rec.getLen() < delta + 20.0 && rec.getLen() > 20.0 - delta);
   }
   @Test public void getLen30() {
-    azzert.that((new Rectangle(10, 20, 50, 50)).getLen(), is(30.0));
+    Rectangle rec = new Rectangle(10, 20, 50, 50);
+    azzert.aye(rec.getLen() < delta + 30.0 && rec.getLen() > 30.0 - delta);
   }
   @Test public void getLenY1BiggerThenY2() {
-    azzert.that((new Rectangle(10.2, 20.6, 50.7, -5.5)).getLen(), is(26.1));
+    Rectangle rec = new Rectangle(10.2, 20.6, 50.7, -5.5);
+    azzert.aye(rec.getLen() < delta + 26.1 && rec.getLen() > 26.1 - delta);
+  }
+  @Test public void getWidth20() {
+    Rectangle rec = new Rectangle(10, 20, 50, 40);
+    azzert.aye(rec.getWidth() < delta + 40 && rec.getWidth() > 40 - delta);
+  }
+  @Test public void geWidth30() {
+    Rectangle rec = new Rectangle(10, 20, 12.4, 50);
+    azzert.aye(rec.getWidth() < delta + 2.4 && rec.getWidth() > 2.4 - delta);
+  }
+  @Test public void getWidthX1BiggerThenX2() {
+    Rectangle rec = new Rectangle(10.2, 20.6, 5.7, -5.5);
+    azzert.aye(rec.getWidth() < delta + 4.5 && rec.getWidth() > 4.5 - delta);
   }
 }
