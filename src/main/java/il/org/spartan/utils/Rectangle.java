bb9508faa8964cb562ed052f2e1213540307f985
max <maxcalderon@campus.technion.ac.il>
2018-11-13 17:16:17 +0200

    #75: added test getLenY1BiggerThenY2

--- src/main/java/il/org/spartan/utils/Rectangle.java
+++ src/main/java/il/org/spartan/utils/Rectangle.java
@@ -1,14 +1,14 @@
 package il.org.spartan.utils;
 
 public class Rectangle {
-  private int y1, y2;
+  private double y1, y2;
   
-  public Rectangle(int x1, int y1, int x2, int y2) {
+  public Rectangle(double x1, double y1, double x2, double y2) {
     this.y1 = y1;
     this.y2 = y2;
   }
 
-  public int getLen() {
-    return this.y2 - this.y1;
+  public double getLen() {
+    return (this.y2 > this.y1) ? (this.y2 - this.y1) : (this.y1 - this.y2) ;
   }
 }
--- src/test/java/il/org/spartan/utils/RectangleTest.java
+++ src/test/java/il/org/spartan/utils/RectangleTest.java
@@ -11,9 +11,12 @@ import fluent.ly.*;
     azzert.notNull(new Rectangle(10,20,30,40));
   }
   @Test public void getLen20() {
-    azzert.that((new Rectangle(10, 20, 50, 40)).getLen(), is(20));
+    azzert.that((new Rectangle(10, 20, 50, 40)).getLen(), is(20.0));
   }
   @Test public void getLen30() {
-    azzert.that((new Rectangle(10, 20, 50, 50)).getLen(), is(30));
+    azzert.that((new Rectangle(10, 20, 50, 50)).getLen(), is(30.0));
+  }
+  @Test public void getLenY1BiggerThenY2() {
+    azzert.that((new Rectangle(10.2, 20.6, 50.7, -5.5)).getLen(), is(26.1));
   }
 }
