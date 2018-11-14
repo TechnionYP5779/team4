e9a02deb5fb220323f893c6f197ff97b9dea0a89
max <maxcalderon@campus.technion.ac.il>
2018-11-14 06:57:22 +0200

    fixed some infos

--- src/main/java/il/org/spartan/utils/Rectangle.java
+++ src/main/java/il/org/spartan/utils/Rectangle.java
@@ -43,18 +43,8 @@ public class Rectangle {
   }
 
   public Rectangle rotation() {
-    if (y2 > y1) {
-      x2 = x1 + y1 - y2;
-      y2 = prevX2 <= x1 ? y1-(x1-prevX2) : y1 + prevX2 - x1;
-    } else {
-      if (x2 > x1) {
-        x2 = x1+(y1-y2);
-        y2 = y1+(prevX2-x1);
-      } else {
-        x2 = x1+(y1-y2);
-        y2 = y1-(x1-prevX2);
-      }
-    }
+    x2 = x1 + y1 - y2;
+    y2 = y1 + prevX2 - x1;
     prevX2 = x2;
     return this;
   }
