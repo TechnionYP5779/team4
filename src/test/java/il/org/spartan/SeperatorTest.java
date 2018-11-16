10526ddb71271f7cfb4d74d180cb2fd30f04acc8
A'meer A'mer <ameeramer@campus.technion.ac.il>
2018-11-16 15:22:09 +0200

    #52: added tests for Separator class

---
+++ src/test/java/il/org/spartan/SeperatorTest.java
@@ -0,0 +1,22 @@
+package il.org.spartan;
+
+import org.junit.*;
+
+import fluent.ly.*;
+import static fluent.ly.azzert.*;
+
+import java.util.*;
+
+@SuppressWarnings("static-method") public class SeperatorTest {
+  @Test public void seperateIntByComma() {
+    azzert.that(Separator.separateBy(new int[] {4, 5}, ","), is("4,5"));
+  }
+  
+  @Test @SuppressWarnings("null") public void seperateObjsByComma() {
+    azzert.that(Separator.separateBy(",", new Double[] { Double.valueOf(4.2), Double.valueOf(5.1) }), is("4.2,5.1"));
+  }
+  
+  @Test public void wrap() {
+    azzert.that(Separator.wrap("\"", Arrays.asList(box.boxInteger(1),box.boxInteger(2),box.boxInteger(3)),","), is("\"1,2,3\""));
+  }
+}
