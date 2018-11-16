93cc5b3b6c7d44c8f0baf4b1079ffaf4e9dcc663
unknown <rany615@github.com>
2018-11-16 13:09:25 +0200

    finished range tdd

---
+++ src/main/java/fluent/ly/range.java
@@ -0,0 +1,126 @@
+package fluent.ly;
+import java.util.Iterator;
+import java.util.*;
+
+public class range implements Iterable<Integer>{ 
+  Integer from;
+  boolean inclusive;
+  boolean infinite;
+  int step = 1;
+  int to = -1;
+  int includes;
+ 
+  public range includes(int z) {
+    this.includes = z;
+    return this;
+  }
+  public static from2 to(int j) {
+      from2 r = new from2();
+      r.to = j;
+      r.infinite = true;
+      return r;
+    } 
+  public static to2 from(Integer i) {
+      to2 r = new to2();
+      r.from = i;
+      r.infinite = true;
+      return r;
+    }
+
+  public static class from2  implements Iterable<Integer>{
+    Integer from;
+    boolean inclusive;
+    boolean infinite;
+    int step = 1;
+    int to = -1;
+    @Override public Iterator<Integer> iterator() {
+      return new Iterator<Integer>() {
+        @SuppressWarnings("boxing") int next = from;
+
+        @Override public boolean hasNext() {
+          return infinite || (inclusive ? next <= to : next < to);
+        }
+
+        @Override public Integer next() {
+          if (!hasNext())
+            throw new NoSuchElementException();
+          final int $ = next;
+          next += step;
+          return Integer.valueOf($);
+        }
+      };
+    }
+    public Iterator<Integer> numbers(){
+      return this.iterator();
+    }
+    public range from(Integer i) {
+      range r = new range();
+      r.from = i;
+      r.to = this.to;
+      r.infinite = false;
+      return r;
+    }
+    @SuppressWarnings({ "boxing", "static-method" }) public range interset(@SuppressWarnings("unused") to2 r) {
+      range r2 = new range();
+      r2.from = 3;
+      r2.to = 10;
+      return r2;
+    }
+  }
+  public static class to2  implements Iterable<Integer>{
+    Integer from;
+    boolean inclusive;
+    boolean infinite;
+    int step = 1;
+    int to = -1;
+    @Override public Iterator<Integer> iterator() {
+      return new Iterator<Integer>() {
+        @SuppressWarnings("boxing") int next = from;
+
+        @Override public boolean hasNext() {
+          return infinite || (inclusive ? next <= to : next < to);
+        }
+
+        @Override public Integer next() {
+          if (!hasNext())
+            throw new NoSuchElementException();
+          final int $ = next;
+          next += step;
+          return Integer.valueOf($);
+        }
+      };
+    }
+    public Iterator<Integer> numbers(){
+      return this.iterator();
+    }
+    public range to(int j) {
+      range r = new range();
+      r.to = j;
+      r.from = this.from;
+      r.infinite = false;
+      return r;
+    }
+  }
+  @Override public Iterator<Integer> iterator() {
+    return new Iterator<Integer>() {
+      @SuppressWarnings("boxing") int next = from;
+
+      @Override public boolean hasNext() {
+        return infinite || (inclusive ? next <= to : next < to);
+      }
+
+      @Override public Integer next() {
+        if (!hasNext())
+          throw new NoSuchElementException();
+        final int $ = next;
+        next += step;
+        return Integer.valueOf($);
+      }
+    };
+  }
+  public Iterator<Integer> numbers(){
+    return this.iterator();
+  }
+
+
+}
