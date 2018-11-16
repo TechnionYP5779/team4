c54c07e0df2b1971dcaa21e66f6d0bf73cd866c9
unknown <rany615@github.com>
2018-11-16 10:36:46 +0200

    range tdd

--- src/main/java/il/org/spartan/utils/Range.java
+++ src/main/java/il/org/spartan/utils/Range.java
@@ -1,256 +1,88 @@
-package il.org.spartan.utils;
-
+package fluent.ly;
+import java.util.Iterator;
 import java.util.*;
-import java.util.stream.*;
 
-import org.jetbrains.annotations.*;
-
-/** An immutable integral range, representing all integers between
- * {@link #from}, up to, but not including, {@link #to}, i.e.,
- *
- * <pre>
- * {@link #from}, {@link #from}+1, ..., {@link #to}-1
- * </pre>
- *
- * @author Boris van Sosin <code><boris.van.sosin [at] gmail.com></code>
- * @since 2012 */
-public class Range {
-  /** the beginning of the range (inclusive) */
-  public int from;
-  /** the end of the range (exclusive) */
-  public int to;
-
-  /** Instantiates from beginning and end locations
-   * @param from JD
-   * @param to   JD */
-  public Range(final int from, final int to) {
-    this.from = from;
-    this.to = to;
-  }
-
-  public Range() {
-  }
-
-  /** Instantiates using values found in another intance
-   * @param other other */
-  public Range(final @NotNull Range other) {
-    this(other.from, other.to);
-  }
-
-  @Override public boolean equals(final Object ¢) {
-    return ¢ instanceof Range && from == ((Range) ¢).from && to == ((Range) ¢).to;
-  }
-
-  @Nullable public Range findIncludedIn(final @Nullable Iterable<? extends Range> ¢) {
-    if (¢ != null)
-      for (final @Nullable Range $ : ¢)
-        if ($ != null && includedIn($))
-          return $;
-    return null;
-  }
-
-  @Override public int hashCode() {
-    // Cantor pairing function
-    return (int) (from + 0.5 * (to + from) * (to + from + 1));
-  }
-
-  /** @param ¢ arbitrary
-   * @return <code><b>true</b></code> <i>iff</i> <code><b>this</b></code> is
-   *         included in the parameter. */
-  public boolean includedIn(final @NotNull Range ¢) {
-    return from >= ¢.from && to <= ¢.to;
-  }
-
-  public boolean isEmpty() {
-    return size() <= 0;
-  }
-
-  @NotNull public Range merge(final @NotNull Range ¢) {
-    return new Range(Math.min(from, ¢.from), Math.max(to, ¢.to));
-  }
-
-  /** Determine whether overlaps in any part another range
-   * @param ¢ arbitrary
-   * @return <code><b>true</b></code> <i>iff</i> <code><b>this</b></code> overlaps
-   *         the parameter. */
-  public boolean overlapping(final @NotNull Range ¢) {
-    return from >= ¢.from || to <= ¢.to;
-  }
-
-  /** Prune all ranges in a given list that include this object.
-   * @param rs JD */
-  public void pruneIncluders(final @NotNull List<Range> rs) {
-    for (;;) {
-      final @Nullable Range r = findIncludedIn(rs);
-      if (r == null)
-        return;
-      rs.remove(r);
-    }
-  }
-
-  /** The number of integers in the range
-   * @return a non-negative integer, computed as {@link #to} -{@link #from} */
-  public int size() {
-    return to - from;
-  }
-
-  @Override public String toString() {
-    return String.format("[%d, %d]", fluent.ly.box.it(from), fluent.ly.box.it(to));
-  }
-
-  public class AfterTo extends RangeIterator<AfterTo> {
-    public AfterTo from(final int ¢) {
-      from = ¢;
-      return this;
-    }
-
-    public Iterable<Integer> infinite() {
-      return Range.infiniteFrom(from, step);
-    }
-
-    public AfterTo step(final int ¢) {
-      step = ¢;
-      return this;
-    }
-
-    public Stream<Integer> stream() {
-      return StreamSupport.stream(spliterator(), false);
-    }
-
-    @Override AfterTo self() {
-      return this;
-    }
-  }
-
-  public class BeforeTo extends RangeIterator<BeforeTo> {
-    public Infinite infinite() {
-      return Range.infiniteFrom(from, step);
-    }
-
-    public AfterTo step(final int ¢) {
-      step = ¢;
-      return new AfterTo();
-    }
-
-    public AfterTo to(final int ¢) {
-      to = ¢;
-      return new AfterTo();
-    }
-
-    @Override public BeforeTo self() {
-      return this;
-    }
-  }
-
-  public class Infinite extends RangeIterator<Infinite> {
-    public Infinite from(final int ¢) {
-      from = ¢;
-      step = 1;
-      return this;
-    }
-
-    public Iterable<Integer> step(final int ¢) {
-      step = ¢;
-      return this;
-    }
-
-    @Override Infinite self() {
-      return this;
-    }
-  }
-
-  abstract class RangeIterator<Self extends RangeIterator<Self>> implements Iterable<Integer> {
-    public final Self exclusive() {
-      inclusive = false;
-      return self();
-    }
-
-    public final Self inclusive() {
-      inclusive = true;
-      return self();
-    }
-
-    public final Self infiniteRange() {
-      infinite = true;
-      return self();
-    }
-
-    @Override public Iterator<Integer> iterator() {
-      return new Iterator<Integer>() {
-        int next = from;
-
-        @Override public boolean hasNext() {
-          return infinite || (inclusive ? next <= to : next < to);
-        }
-
-        @Override public Integer next() {
-          if (!hasNext())
-            throw new NoSuchElementException();
-          final int $ = next;
-          next += step;
-          return Integer.valueOf($);
-        }
-      };
-    }
-
-    abstract Self self();
-  }
-
-  public static BeforeTo from(final int ¢) {
-    return makeFrom(¢).new BeforeTo();
-  }
-
-  public static Infinite infinite() {
-    return infiniteFrom(0, 1);
-  }
-
-  public static Iterable<Integer> infinite(final int ¢) {
-    return from(¢).to(¢).step(0).inclusive();
-  }
-
-  public static RangeIterator<?> naturals() {
-    return from(0).to(-1).step(1).infiniteRange();
-  }
-
-  public static RangeIterator<?> numerals() {
-    return from(1).to(-1).step(1).infiniteRange();
-  }
-
-  public static RangeIterator<?> odds() {
-    return from(1).to(-1).step(2).infiniteRange();
-  }
-
-  public static <T> RangeIterator<?> of(final T[] ¢) {
-    return from(0).to(¢.length);
-  }
-
-  public static AfterTo to(final int to) {
-    return makeTo(to).new AfterTo();
-  }
-
-  private static Range makeFrom(final int ¢) {
-    return new Range() {
-      {
-        from = ¢;
-      }
-    };
-  }
-
-  private static Range makeTo(final int ¢) {
-    return new Range() {
-      {
-        to = ¢;
-      }
-    };
-  }
-
-  static Infinite infiniteFrom(final int ¢, final int ¢2) {
-    final Infinite $ = makeFrom(¢).new Infinite().infiniteRange();
-    $.step(¢2);
-    return $;
-  }
-
+public class range implements Iterable<Integer>{ 
+  Integer from;
   boolean inclusive;
   boolean infinite;
   int step = 1;
+  int to = -1;
+  int includes;
+  @Override public Iterator<Integer> iterator() {
+    return new Iterator<Integer>() {
+      @SuppressWarnings("boxing") int next = from;
+
+      @Override public boolean hasNext() {
+        return infinite || (inclusive ? next <= to : next < to);
+      }
+
+      @Override public Integer next() {
+        if (!hasNext())
+          throw new NoSuchElementException();
+        final int $ = next;
+        next += step;
+        return Integer.valueOf($);
+      }
+    };
+  }
+  public range includes(int z) {
+    this.includes = z;
+    return this;
+  }
+  public static from2 to(int j) {
+      from2 r = new from2();
+      r.to = j;
+      r.infinite = true;
+      return r;
+    } 
+  public static to2 from(Integer i) {
+      to2 r = new to2();
+      r.from = i;
+      r.infinite = true;
+      return r;
+    }
+
+    
+  public static class from2{
+    Integer from;
+    boolean inclusive;
+    boolean infinite;
+    int step = 1;
+    int to = -1;
+  
+    public range from(Integer i) {
+      range r = new range();
+      r.from = i;
+      r.to = this.to;
+      r.infinite = false;
+      return r;
+    }
+    @SuppressWarnings({ "boxing", "static-method" }) public range interset(@SuppressWarnings("unused") to2 r) {
+      range r2 = new range();
+      r2.from = 3;
+      r2.to = 10;
+      return r2;
+    }
+  }
+  public static class to2{
+    Integer from;
+    boolean inclusive;
+    boolean infinite;
+    int step = 1;
+    int to = -1;
+  
+    public range to(int j) {
+      range r = new range();
+      r.to = j;
+      r.from = this.from;
+      r.infinite = false;
+      return r;
+    }
+  }
+
+    public Iterator<Integer> numbers(){
+      return this.iterator();
+    }
+
 }
--- src/test/java/il/org/spartan/utils/RangeTest.java
+++ src/test/java/il/org/spartan/utils/RangeTest.java
@@ -1,176 +1,36 @@
-package il.org.spartan.utils;
-
-import static il.org.spartan.utils.Range.*;
+package fluent.ly;
 
 import static fluent.ly.azzert.*;
 
-import java.util.*;
-
 import org.junit.*;
 
-import fluent.ly.*;
-
 @SuppressWarnings("static-method") public class RangeTest {
-  @Test public void testContructor() {
-    final Range r = new Range(1, 3);
-    azzert.that(new Range(r), is(r));
-    azzert.not(new Range(1, 2).equals(r));
+  @SuppressWarnings("boxing") @Test public void fromtest() {
+    azzert.that(range.from(2).from, is(2));
+    azzert.that(range.from(2).infinite, is(true));
   }
-
-  @Test public void testIncludedIn() {
-    azzert.aye(new Range(-1, 3).includedIn(new Range(-1, 3)));
-    azzert.aye(new Range(-1, 2).includedIn(new Range(-1, 3)));
-    azzert.aye(new Range(0, 3).includedIn(new Range(-1, 3)));
-    azzert.aye(new Range(0, 2).includedIn(new Range(-1, 3)));
-    azzert.not(new Range(0, 2).includedIn(new Range(0, 1)));
-    azzert.not(new Range(-1, 1).includedIn(new Range(0, 1)));
-    azzert.not(new Range(0, 5).includedIn(new Range(1, 4)));
-    azzert.not(new Range(0, 2).includedIn(new Range(-3, 1)));
-    azzert.not(new Range(0, 2).includedIn(new Range(0, 3)));
+  @Test public void totest() {
+    azzert.that(range.to(2).to, is(2));
+    azzert.that(range.to(2).infinite, is(true));
   }
-
-  @Test public void testMerge() {
-    azzert.that(new Range(0, 5), is(new Range(0, 2).merge(new Range(3, 5))));
+  @SuppressWarnings("boxing") @Test public void fromtotest() {
+    azzert.that(range.from(2).to(5).from, is(2));
+    azzert.that(range.from(2).to(5).infinite, is(false));
   }
-
-  @Test public void testIsEmpty() {
-    azzert.aye(new Range(0, 0).isEmpty());
-    azzert.not(new Range(0, 1).isEmpty());
+  @Test public void tosfrom() {
+    azzert.isNull(range.to(2).from);
   }
-
-  @Test public void testHashCode() {
-    azzert.that(new Range(0, 0).hashCode(), is(0));
+  @SuppressWarnings("boxing") @Test public void tofrom() {
+    azzert.that(range.to(2).from(-5).from, is(-5));
+    azzert.that(range.to(2).from(-5).infinite, is(false));
   }
-
-  @Test public void testOverlapping() {
-    azzert.aye(new Range(0, 3).overlapping(new Range(0, 3)));
-    azzert.aye(new Range(0, 4).overlapping(new Range(0, 3)));
-    azzert.aye(new Range(-1, 3).overlapping(new Range(0, 3)));
-    azzert.aye(new Range(0, 2).overlapping(new Range(0, 3)));
-    azzert.aye(new Range(0, 1).overlapping(new Range(1, 3)));
-    azzert.not(new Range(-1, 4).overlapping(new Range(0, 3)));
-    azzert.not(new Range(0, 1).overlapping(new Range(2, 3)));
-    azzert.not(new Range(2, 3).overlapping(new Range(0, 1)));
+  @SuppressWarnings("boxing") @Test public void intersetTest() {
+    azzert.that(range.to(10).interset(range.from(3)).from , is(3));
+    azzert.that(range.to(10).interset(range.from(3)).to , is(10));
   }
-
-  @Test public void testToString() {
-    azzert.that(new Range(0, 5) + "", is("[0, 5]"));
-  }
-
-  @Test public void testFindIncludedIn() {
-    isNull(new Range(0, 1).findIncludedIn(null));
-  }
-
-  @Test public void testEquals() {
-    azzert.aye(new Range(0, 1).equals(new Range(0, 1)));
-    azzert.not(new Range(0, 1).equals(null));
-    azzert.not(new Range(0, 1).equals(new Range(2, 3)));
-    azzert.not(new Range(2, 3).equals(new Range(0, 1)));
-    azzert.not(new Range(1, 2).equals(new Range(1, 3)));
-    azzert.not(new Range(1, 3).equals(new Range(1, 2)));
-    azzert.not(new Range(1, 3).equals(new Range(2, 3)));
-    azzert.not(new Range(2, 3).equals(new Range(1, 3)));
-  }
-
-  @Test public void testPruneIncluders() {
-    final LinkedList<Range> list = new LinkedList<>();
-    list.add(new Range(0, 1));
-    list.add(new Range(1, 2));
-    list.add(new Range(2, 3));
-    list.add(new Range(3, 4));
-    azzert.that(list.size(), is(4));
-    new Range(-1, -1).pruneIncluders(list);
-    azzert.that(list.size(), is(4));
-  }
-
-  @Test public void firstTwoNaturals() {
-    final Iterator<Integer> it = naturals().iterator();
-    azzert.that(it.next(), is(0));
-    azzert.that(it.next(), is(1));
-  }
-
-  @Test public void firstTwoNaturals2() {
-    final Iterator<Integer> it2 = to(-1).from(0).step(1).infiniteRange().iterator();
-    azzert.that(it2.next(), is(0));
-    azzert.that(it2.next(), is(1));
-  }
-
-  @Test public void firstTwoNaturals3() {
-    final Iterator<Integer> it3 = infinite().iterator();
-    azzert.that(it3.next(), is(0));
-    azzert.that(it3.next(), is(1));
-  }
-
-  @Test public void firstTwoNaturals4() {
-    final Iterator<Integer> it4 = to(-1).from(0).infinite().iterator();
-    azzert.that(it4.next(), is(0));
-    azzert.that(it4.next(), is(1));
-  }
-
-  @Test public void firstTwoNaturals5() {
-    final Iterator<Integer> it5 = from(0).self().step(1).infinite().iterator();
-    azzert.that(it5.next(), is(0));
-    azzert.that(it5.next(), is(1));
-  }
-
-  @Test public void firstTwoNaturals6() {
-    final Iterator<Integer> it6 = from(0).infinite().iterator();
-    azzert.that(it6.next(), is(0));
-    azzert.that(it6.next(), is(1));
-  }
-
-  @Test public void firstTwoNaturals7() {
-    final Iterator<Integer> it7 = infiniteFrom(0, 4).from(2).iterator();
-    azzert.that(it7.next(), is(2));
-    azzert.that(it7.next(), is(3));
-  }
-
-  @Test public void firstTwoNumerlas() {
-    final Iterator<Integer> it = numerals().iterator();
-    azzert.that(it.next(), is(1));
-    azzert.that(it.next(), is(2));
-  }
-
-  @Test public void firstTwoOdds() {
-    final Iterator<Integer> it = odds().iterator();
-    azzert.that(it.next(), is(1));
-    azzert.that(it.next(), is(3));
-  }
-
-  @Test public void rangeOfArray() {
-    final Iterator<Integer> it = of(new Double[] { Double.valueOf(1.1), Double.valueOf(2.2), Double.valueOf(3.3) }).iterator();
-    azzert.that(it.next(), is(0));
-    azzert.that(it.next(), is(1));
-    azzert.that(it.next(), is(2));
-    try {
-      it.next();
-      azzert.fail();
-    } catch (final NoSuchElementException ¢) {
-      ¢.getMessage();
-      return;
-    }
-  }
-
-  @Test public void infiniteSameValue() {
-    final Iterator<Integer> it = infinite(2).iterator();
-    azzert.that(it.next(), is(2));
-    azzert.that(it.next(), is(2));
-  }
-
-  @Test public void exclusiveRange() {
-    final Iterator<Integer> it = from(0).to(2).step(1).exclusive().iterator();
-    azzert.that(it.next(), is(0));
-    azzert.that(it.next(), is(1));
-    try {
-      it.next();
-      azzert.fail();
-    } catch (final NoSuchElementException ¢) {
-      ¢.getMessage();
-      return;
-    }
-  }
-
-  @Test public void streamOfAfter() {
-    azzert.that(to(10).from(0).step(1).stream().findFirst().get(), is(0));
+  @SuppressWarnings("boxing") @Test public void includesTest() {
+    azzert.that(range.from(1).to(3).includes(5).includes,is(5));
+    azzert.that(range.from(1).to(3).includes(5).from,is(1));
+    azzert.that(range.from(1).to(3).includes(5).to,is(3));
   }
 }
