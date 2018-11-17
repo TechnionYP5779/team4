72f7bb8b9f5bd75cb2628bee4dd9d2ad06b84b22
Ron Marcovich <ron4548@gmail.com>
2018-11-17 23:06:08 +0200

    reset class Range #108

--- src/main/java/il/org/spartan/utils/Range.java
+++ src/main/java/il/org/spartan/utils/Range.java
@@ -2,93 +2,95 @@ package il.org.spartan.utils;
 
 import java.util.*;
 
-import fluent.ly.*;
+import org.jetbrains.annotations.*;
 
-public class range implements Iterable<Integer> {
-  Integer from;
-  boolean inclusive;
-  boolean infinite;
-  int step = 1;
-  int to = -1;
-  int includes;
+/** An immutable integral range, representing all integers between
+ * {@link #from}, up to, but not including, {@link #to}, i.e.,
+ *
+ * <pre>
+ * {@link #from}, {@link #from}+1, ..., {@link #to}-1
+ * </pre>
+ *
+ * @author Boris van Sosin <code><boris.van.sosin [at] gmail.com></code>
+ * @since 2012 */
+public class Range {
+  /** the beginning of the range (inclusive) */
+  public final int from;
+  /** the end of the range (exclusive) */
+  public final int to;
 
-  @Override public Iterator<Integer> iterator() {
-    return new Iterator<Integer>() {
-      @SuppressWarnings("null") int next = unbox.unboxInteger(from);
-
-      @Override public boolean hasNext() {
-        return infinite || (inclusive ? next <= to : next < to);
-      }
-
-      @Override public Integer next() {
-        if (!hasNext())
-          throw new NoSuchElementException();
-        final int $ = next;
-        next += step;
-        return Integer.valueOf($);
-      }
-    };
+  /** Instantiates from beginning and end locations
+   * @param from JD
+   * @param to   JD */
+  public Range(final int from, final int to) {
+    this.from = from;
+    this.to = to;
   }
 
-  public range includes(final int z) {
-    this.includes = z;
-    return this;
+  /** Instantiates using values found in another intance
+   * @param other other */
+  public Range(final @NotNull Range other) {
+    this(other.from, other.to);
   }
 
-  public static from2 to(final int j) {
-    final from2 $ = new from2();
-    $.to = j;
-    $.infinite = true;
-    return $;
+  @Override public boolean equals(final Object ¢) {
+    return ¢ instanceof Range && from == ((Range) ¢).from && to == ((Range) ¢).to;
   }
 
-  public static to2 from(final Integer ¢) {
-    final to2 $ = new to2();
-    $.from = ¢;
-    $.infinite = true;
-    return $;
+  @Nullable public Range findIncludedIn(final @Nullable Iterable<? extends Range> ¢) {
+    if (¢ != null)
+      for (final @Nullable Range $ : ¢)
+        if ($ != null && includedIn($))
+          return $;
+    return null;
   }
 
-  public static class from2 {
-    Integer from;
-    boolean inclusive;
-    boolean infinite;
-    int step = 1;
-    int to = -1;
-
-    public range from(final Integer ¢) {
-      final range $ = new range();
-      $.from = ¢;
-      $.to = this.to;
-      $.infinite = false;
-      return $;
-    }
-
-    @SuppressWarnings("static-method") public range interset(@SuppressWarnings("unused") final to2 r) {
-      final range $ = new range();
-      $.from = box.boxInteger(3);
-      $.to = 10;
-      return $;
-    }
+  @Override public int hashCode() {
+    // Cantor pairing function
+    return (int) (from + 0.5 * (to + from) * (to + from + 1));
   }
 
-  public static class to2 {
-    Integer from;
-    boolean inclusive;
-    boolean infinite;
-    int step = 1;
-    int to = -1;
+  /** @param ¢ arbitrary
+   * @return <code><b>true</b></code> <i>iff</i> <code><b>this</b></code> is
+   *         included in the parameter. */
+  public boolean includedIn(final @NotNull Range ¢) {
+    return from >= ¢.from && to <= ¢.to;
+  }
 
-    public range to(final int j) {
-      final range $ = new range();
-      $.to = j;
-      $.from = this.from;
-      $.infinite = false;
-      return $;
+  public boolean isEmpty() {
+    return size() <= 0;
+  }
+
+  @NotNull public Range merge(final @NotNull Range ¢) {
+    return new Range(Math.min(from, ¢.from), Math.max(to, ¢.to));
+  }
+
+  /** Determine whether overlaps in any part another range
+   * @param ¢ arbitrary
+   * @return <code><b>true</b></code> <i>iff</i> <code><b>this</b></code> overlaps
+   *         the parameter. */
+  public boolean overlapping(final @NotNull Range ¢) {
+    return from >= ¢.from || to <= ¢.to;
+  }
+
+  /** Prune all ranges in a given list that include this object.
+   * @param rs JD */
+  public void pruneIncluders(final @NotNull List<Range> rs) {
+    for (;;) {
+      final @Nullable Range r = findIncludedIn(rs);
+      if (r == null)
+        return;
+      rs.remove(r);
     }
   }
 
-  public Iterator<Integer> numbers() {
-    return this.iterator();
+  /** The number of integers in the range
+   * @return a non-negative integer, computed as {@link #to} -{@link #from} */
+  public int size() {
+    return to - from;
+  }
+
+  @Override public String toString() {
+    return String.format("[%d, %d]", fluent.ly.box.it(from), fluent.ly.box.it(to));
   }
 }
