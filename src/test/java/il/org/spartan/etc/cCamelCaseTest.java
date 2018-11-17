6d8b168e7f41815a55c7ed412d2bbec1e7c70312
Moshe Sebag <moshesab8@gmail.com>
2018-11-17 22:13:40 +0200

    Tests for /etc/cCamelCaseTest were added

---
+++ src/test/java/il/org/spartan/etc/cCamelCaseTest.java
@@ -0,0 +1,21 @@
+package il.org.spartan.etc;
+
+import static fluent.ly.azzert.*;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+@SuppressWarnings("static-method") public class cCamelCaseTest {
+  @Test public void lastComponent() {
+    azzert.that(cCamelCase.lastComponent(String.class), is("String")); 
+  }
+  @Test public void lastComponent2() {
+    azzert.that(cCamelCase.lastComponent("String"), is("String")); 
+  }
+  @Test public void usefulTypeName() {
+    assert cCamelCase.usefulTypeName("String"); 
+    assert !cCamelCase.usefulTypeName("S"); 
+    assert cCamelCase.usefulTypeName("s"); 
+  }
+}
