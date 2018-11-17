7913e3088193757c041b6e7ec793fac8f93c7d53
Ron Marcovich <ron4548@gmail.com>
2018-11-17 10:01:56 +0200

    moved prune.TEST1 to test hierarchy #120

--- src/main/java/fluent/ly/prune.java
+++ src/main/java/fluent/ly/prune.java
@@ -58,7 +58,7 @@ import org.junit.*;
    * @param   <T> type of elements in the input array.
    * @param ¢ an array of values.
    * @return an array of size 0 of elements of type <code>T</code>. */
-  private static <T> T[] shrink(final T @NotNull [] ¢) {
+  static <T> T[] shrink(final T @NotNull [] ¢) {
     return Arrays.copyOf(¢, 0);
   }
 
@@ -74,70 +74,7 @@ import org.junit.*;
   /** A JUnit test class for the enclosing class.
    * @author Yossi Gil, the Technion.
    * @since 27/08/2008 */
-  @SuppressWarnings({ "static-method", "synthetic-access" }) //
-  public static class TEST1 {
-    @Nullable final String[] alternatingArray = new @Nullable String[] { null, "A", null, null, "B", null, null, null, "C", null };
-    @NotNull final String[] nonNullArray = { "1", "2", "4" };
-    @NotNull private final List<String> sparseCollection = as.list(null, null, null, null, null, "A", null, null, null, "B", null, "C", null, null,
-        null, null, null);
-
-    @Test public void nullsNotNullArrayLength() {
-      azzert.that(nonNullArray.length, is(nulls(nonNullArray).length));
-    }
-
-    @Test public void nullsNullArrayItems() {
-      azzert.that(nulls(nonNullArray)[0], is("1"));
-      azzert.that(nulls(nonNullArray)[1], is("2"));
-      azzert.that(nulls(nonNullArray)[2], is("4"));
-    }
-
-    @Test public void nullsPruneArrayAltenatingItems() {
-      azzert.that(nulls(alternatingArray)[0], is("A"));
-      azzert.that(nulls(alternatingArray)[1], is("B"));
-      azzert.that(nulls(alternatingArray)[2], is("C"));
-    }
-
-    @Test public void nullsPruneArrayAltenatingLength() {
-      azzert.that(nulls(alternatingArray).length, is(3));
-    }
-
-    @Test public void nullsPruneSparseCollectionContents() {
-      final String[] a = nulls(sparseCollection).toArray(new String[3]);
-      azzert.that(a[0], is("A"));
-      azzert.that(a[1], is("B"));
-      azzert.that(a[2], is("C"));
-      azzert.that(a.length, is(3));
-    }
-
-    @Test public void nullsPruneSparseCollectionLength() {
-      azzert.that(nulls(sparseCollection).size(), is(3));
-    }
-
-    @Test public void nullsPrunNotNull() {
-      azzert.notNull(prune.nulls(sparseCollection));
-    }
-
-    @Test public void shrinkArray() {
-      azzert.that(shrink(new Object[10]).length, is(0));
-    }
-
-    @Test public void shrinkEmptyArray() {
-      azzert.that(shrink(new Object[10]).length, is(0));
-    }
-
-    @Test public void whitesEmptyArray() {
-      azzert.that(prune.whites().length, is(0));
-    }
-
-    @Test public void whitesEmptyList() {
-      azzert.that(prune.whites().length, is(0));
-    }
-  }
-
-  /** A JUnit test class for the enclosing class.
-   * @author Yossi Gil, the Technion.
-   * @since 27/08/2008 */
-  @SuppressWarnings({ "static-method", "synthetic-access" }) public static class TEST2 {
+  @SuppressWarnings("static-method") public static class TEST2 {
     @NotNull final String @NotNull [] alternatingArray = new String[] { null, "A", null, null, "B", null, null, null, "C", null };
     @NotNull final String @NotNull [] nonNullArray = { "1", "2", "4" };
     @NotNull final Supplier<List<String>> x = () -> {
---
+++ src/test/java/fluent/ly/PruneTest1.java
@@ -0,0 +1,71 @@
+package fluent.ly;
+
+import static fluent.ly.azzert.*;
+
+import java.util.*;
+
+import org.jetbrains.annotations.*;
+import org.junit.*;
+
+/** A JUnit test class for the enclosing class.
+ * @author Yossi Gil, the Technion.
+ * @since 27/08/2008 */
+@SuppressWarnings("static-method")
+public class PruneTest1 {
+  @Nullable final String @NotNull[] alternatingArray = new @Nullable String[] { null, "A", null, null, "B", null, null, null, "C", null };
+  @NotNull final String @NotNull[] nonNullArray = { "1", "2", "4" };
+  @NotNull private final List<String> sparseCollection = as.list(null, null, null, null, null, "A", null, null, null, "B", null, "C", null, null,
+      null, null, null);
+
+  @Test public void nullsNotNullArrayLength() {
+    azzert.that(nonNullArray.length, is(prune.nulls(nonNullArray).length));
+  }
+
+  @Test public void nullsNullArrayItems() {
+    azzert.that(prune.nulls(nonNullArray)[0], is("1"));
+    azzert.that(prune.nulls(nonNullArray)[1], is("2"));
+    azzert.that(prune.nulls(nonNullArray)[2], is("4"));
+  }
+
+  @Test public void nullsPruneArrayAltenatingItems() {
+    azzert.that(prune.nulls(alternatingArray)[0], is("A"));
+    azzert.that(prune.nulls(alternatingArray)[1], is("B"));
+    azzert.that(prune.nulls(alternatingArray)[2], is("C"));
+  }
+
+  @Test public void nullsPruneArrayAltenatingLength() {
+    azzert.that(prune.nulls(alternatingArray).length, is(3));
+  }
+
+  @Test public void nullsPruneSparseCollectionContents() {
+    final String[] a = prune.nulls(sparseCollection).toArray(new String[3]);
+    azzert.that(a[0], is("A"));
+    azzert.that(a[1], is("B"));
+    azzert.that(a[2], is("C"));
+    azzert.that(a.length, is(3));
+  }
+
+  @Test public void nullsPruneSparseCollectionLength() {
+    azzert.that(prune.nulls(sparseCollection).size(), is(3));
+  }
+
+  @Test public void nullsPrunNotNull() {
+    azzert.notNull(prune.nulls(sparseCollection));
+  }
+
+  @Test public void shrinkArray() {
+    azzert.that(prune.shrink(new Object[10]).length, is(0));
+  }
+
+  @Test public void shrinkEmptyArray() {
+    azzert.that(prune.shrink(new Object[10]).length, is(0));
+  }
+
+  @Test public void whitesEmptyArray() {
+    azzert.that(prune.whites().length, is(0));
+  }
+
+  @Test public void whitesEmptyList() {
+    azzert.that(prune.whites().length, is(0));
+  }
+}
