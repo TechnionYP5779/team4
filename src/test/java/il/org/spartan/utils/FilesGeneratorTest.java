486a93a3a7fc695bc9eb21a21cde02da6d413b05
Ron Marcovich <ron4548@gmail.com>
2018-11-17 12:53:01 +0200

    Tests for class FilesGenerator #52

---
+++ src/test/java/il/org/spartan/utils/FilesGeneratorTest.java
@@ -0,0 +1,31 @@
+package il.org.spartan.utils;
+
+import java.io.*;
+import java.util.*;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+public class FilesGeneratorTest {
+  
+  File resDir;
+  File emptyDir;
+  List<String> filesNames = as.list(new String[] {"f1", "f2"});
+  
+  @Before public void testInit() {
+    resDir = new File(getClass().getClassLoader().getResource("dirWithTwoFiles").getPath());
+  }
+  
+  @Test public void testDirectoryIterator() {
+    
+    azzert.isNull(FilesGenerator.directoryIterator(null));
+    azzert.isNull(FilesGenerator.directoryIterator(resDir.listFiles()[0]));
+    
+    Iterator<File> it = FilesGenerator.directoryIterator(resDir);
+    assert it != null;
+    while (it.hasNext())
+      filesNames.contains(it.next().getName());
+  }
+  
+}
