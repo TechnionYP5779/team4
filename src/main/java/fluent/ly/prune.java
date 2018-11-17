b0fdd24405dce74df8a19433118b652760766831
Ron Marcovich <ron4548@gmail.com>
2018-11-17 10:11:27 +0200

    moved prune.TEST2 to test hierarchy #120

--- src/main/java/fluent/ly/prune.java
+++ src/main/java/fluent/ly/prune.java
@@ -3,13 +3,9 @@ package fluent.ly;
 
 import static il.org.spartan.Utils.*;
 
-import static fluent.ly.azzert.*;
-
 import java.util.*;
-import java.util.function.*;
 
 import org.jetbrains.annotations.*;
-import org.junit.*;
 
 /** A <b>Utility class</b> providing functions to remove
  * <code><b>null</b></code> elements from arrays and iterable collections. For
@@ -69,95 +65,5 @@ import org.junit.*;
         if (¢ != null)
           accumulate.to($).add((¢ + "").trim());
     return asArrray($);
-  }
-
-  /** A JUnit test class for the enclosing class.
-   * @author Yossi Gil, the Technion.
-   * @since 27/08/2008 */
-  @SuppressWarnings("static-method") public static class TEST2 {
-    @NotNull final String @NotNull [] alternatingArray = new String[] { null, "A", null, null, "B", null, null, null, "C", null };
-    @NotNull final String @NotNull [] nonNullArray = { "1", "2", "4" };
-    @NotNull final Supplier<List<String>> x = () -> {
-      final List<String> $ = an.empty.list();
-      $.add(null);
-      $.add(null);
-      $.add(null);
-      $.add(null);
-      $.add(null);
-      $.add("A");
-      $.add(null);
-      $.add(null);
-      $.add(null);
-      $.add("B");
-      $.add(null);
-      $.add("C");
-      $.add(null);
-      $.add(null);
-      $.add(null);
-      $.add(null);
-      return $;
-    };
-    @NotNull private final List<String> sparseCollection = new Supplier<List<String>>() {
-      @Override @NotNull public List<String> get() {
-        @NotNull final List<String> $ = an.empty.list();
-        $.add(null);
-        $.add(null);
-        $.add(null);
-        $.add(null);
-        $.add(null);
-        $.add("A");
-        $.add(null);
-        $.add(null);
-        $.add(null);
-        $.add("B");
-        $.add(null);
-        $.add("C");
-        $.add(null);
-        $.add(null);
-        $.add(null);
-        $.add(null);
-        return $;
-      }
-    }.get();
-
-    @Test public void testNotNullArrayItems() {
-      azzert.that(nulls(nonNullArray)[0], is("1"));
-      azzert.that(nulls(nonNullArray)[1], is("2"));
-      azzert.that(nulls(nonNullArray)[2], is("4"));
-    }
-
-    @Test public void testNotNullArrayLength() {
-      azzert.that(nulls(nonNullArray).length, is(nonNullArray.length));
-    }
-
-    @Test public void testPruneArrayAltenatingItems() {
-      azzert.that(nulls(alternatingArray)[0], is("A"));
-      azzert.that(nulls(alternatingArray)[1], is("B"));
-      azzert.that(nulls(alternatingArray)[2], is("C"));
-    }
-
-    @Test public void testPruneArrayAltenatingLength() {
-      azzert.that(nulls(alternatingArray).length, is(3));
-    }
-
-    @Test public void testPruneSparseCollectionContents() {
-      final @NotNull String[] a = nulls(sparseCollection).toArray(new String[3]);
-      azzert.that(a[0], is("A"));
-      azzert.that(a[1], is("B"));
-      azzert.that(a[2], is("C"));
-      azzert.that(a.length, is(3));
-    }
-
-    @Test public void testPruneSparseCollectionLength() {
-      azzert.that(nulls(sparseCollection).size(), is(3));
-    }
-
-    @Test public void testPrunNotNull() {
-      azzert.notNull(nulls(sparseCollection));
-    }
-
-    @Test public void testShrink() {
-      azzert.that(shrink(new Object[10]).length, is(0));
-    }
   }
 }
