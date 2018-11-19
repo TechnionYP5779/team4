76e2bc4e07e71a0f30c11d16f4e69149869f2e9f
Yuval Ron <yuvalron@hotmail.com>
2018-11-19 12:50:44 +0200

    #52 Added YesTest

---
+++ src/test/java/fluent/ly/YesTest.java
@@ -0,0 +1,42 @@
+package fluent.ly;
+
+import static fluent.ly.azzert.*;
+
+import org.jetbrains.annotations.*;
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class YesTest {
+  @Test public void ignoringBoolean() {
+    assert yes.ignoring(false);
+    assert yes.ignoring(true);
+  }
+  
+  @Test public void ignoringDouble() {
+    assert yes.ignoring(0.0);
+    assert yes.ignoring(1.0);
+    assert yes.ignoring(-1.0);
+  }
+  
+  @Test public void ignoringLong() {
+    assert yes.ignoring(0);
+    assert yes.ignoring(1);
+    assert yes.ignoring(-1);
+  }
+  
+  public class MyThread extends Thread {
+    @Override public void run(){
+      // empty
+    }
+  }
+  
+  @Test public void forgettingRunnable() {
+    assert yes.forgetting(new MyThread());
+  }
+  
+  @Test public void forgettingObjects() {
+    assert yes.forgetting("1", "2");
+    assert yes.forgetting("1", "2", "3");
+    assert yes.forgetting(Integer.valueOf(1), "2");
+    assert yes.forgetting("1");
+  }
+}
