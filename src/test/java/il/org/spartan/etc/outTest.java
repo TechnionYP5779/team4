5612a7aa47559f79bb005132eecdbba456aca7dc
Moshe Sebag <moshesab8@gmail.com>
2018-11-19 20:13:52 +0200

    more tests

--- src/test/java/il/org/spartan/etc/outTest.java
+++ src/test/java/il/org/spartan/etc/outTest.java
@@ -60,6 +60,14 @@ import fluent.ly.*;
     out.out_collection("koko", collection);
     azzert.that(outContent + "", is("Only 1 koko: Hello\n"));
   }
+  @Test public void out_collection22() {
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    final Collection<Object> collection = new ArrayList<>();
+    collection.add("apple");
+    out.out_collection("fruit", collection);
+    azzert.that(outContent + "", is("Only 1 fruit: apple\n"));
+  }
 
   @Test public void out_collection3() {
     final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -69,6 +77,16 @@ import fluent.ly.*;
     collection.add("Bye");
     out.out_collection("koko", collection);
     azzert.that(outContent + "", is("Total of 2 koko:\n\t 1) Hello\n\t 2) Bye\n"));
+  }
+  
+  @Test public void out_collection33() {
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    final Collection<Object> collection = new ArrayList<>();
+    collection.add("banana");
+    collection.add("apple");
+    out.out_collection("fruits", collection);
+    azzert.that(outContent + "", is("Total of 2 fruits:\n\t 1) banana\n\t 2) apple\n"));
   }
 
   @Test public void out_collection4() {
@@ -81,7 +99,16 @@ import fluent.ly.*;
     azzert.that(outContent + "", is(
         "Total of 40 koko:\n\t 1) a0\n\t 2) a1\n\t 3) a2\n\t 4) a3\n\t 5) a4\n\t 6) a5\n\t 7) a6\n\t 8) a7\n\t 9) a8\n\t10) a9\n\t11) a10\n\t12) a11\n\t13) a12\n\t14) a13\n\t15) a14\n\t16) a15\n\t17) a16\n\t18) a17\n\t19) a18\n\t20) a19\n\t...\n"));
   }
-
+  @Test public void out_collection44() {
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    final Collection<Object> collection = new ArrayList<>();
+    for (int ¢ = 0; ¢ < 40; ++¢)
+      collection.add("a" + ¢);
+    out.out_collection("fruits", collection);
+    azzert.that(outContent + "", is(
+        "Total of 40 fruits:\n\t 1) a0\n\t 2) a1\n\t 3) a2\n\t 4) a3\n\t 5) a4\n\t 6) a5\n\t 7) a6\n\t 8) a7\n\t 9) a8\n\t10) a9\n\t11) a10\n\t12) a11\n\t13) a12\n\t14) a13\n\t15) a14\n\t16) a15\n\t17) a16\n\t18) a17\n\t19) a18\n\t20) a19\n\t...\n"));
+  }
   @Test public void out_integer() {
     final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
@@ -95,7 +122,12 @@ import fluent.ly.*;
     out.out_object("hi", null);
     azzert.that(outContent + "", is("No hi\n"));
   }
-
+  @Test public void out_object22() {
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    out.out_object("fruit", null);
+    azzert.that(outContent + "", is("No fruit\n"));
+  }
   @Test public void out_object2() {
     final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
