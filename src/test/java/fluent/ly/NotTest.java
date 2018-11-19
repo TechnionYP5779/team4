5545765afa904df52db3f0c7a05752ae589df884
Yuval Ron <yuvalron@hotmail.com>
2018-11-19 13:06:34 +0200

    #52 Added unit test named NotTest

---
+++ src/test/java/fluent/ly/NotTest.java
@@ -0,0 +1,21 @@
+package fluent.ly;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class NotTest {
+  @Test public void nill1() {
+    assert !not.nil(null);
+  }
+  
+  @Test public void nill2() {
+    assert not.nil(Integer.valueOf(1));
+  }
+  
+  @Test public void in1() {
+    assert not.in("");
+  }
+  
+  @Test public void in2() {
+    assert !not.in("", "");
+  }
+}
