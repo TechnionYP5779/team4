17c4ef00c38f93b50701cbcdf906203080ab8e5b
A'meer A'mer <ameeramer@campus.technion.ac.il>
2018-11-19 18:20:28 +0200

    #52: added tests for class Rule

---
+++ src/test/java/il/org/spartan/utils/RuleTest.java
@@ -0,0 +1,61 @@
+package il.org.spartan.utils;
+
+import java.io.*;
+import java.util.logging.*;
+
+import org.jetbrains.annotations.*;
+import org.junit.*;
+
+import fluent.ly.*;
+import static fluent.ly.azzert.*;
+import static il.org.spartan.utils.Rule.*;
+@SuppressWarnings("static-method") public class RuleTest {
+  static final LogHandler lh = (new RuleTest()).new LogHandler();
+  
+  class LogHandler extends Handler
+  {
+      String msg;
+
+      public String get() {
+          return msg;
+      }    
+
+      @Override public void publish(LogRecord record) {
+          msg = record.getMessage();
+      }
+
+      @Override public void close(){/**/}
+      @Override public void flush(){/**/}
+  }
+  
+  @Before
+  public void setup() {
+    Logger globalLogger = Logger.getLogger("global");
+    Handler[] handlers = globalLogger.getHandlers();
+    for(Handler ¢ : handlers)
+      globalLogger.removeHandler(¢);
+    globalLogger.addHandler(lh);
+    
+    System.setErr(new PrintStream(new ByteArrayOutputStream()));
+  }
+  
+  @Test public void description1() { 
+    assert ((new Rule<@Nullable Integer, Integer>() {
+
+      @Override public Integer apply(@Nullable Integer ¢) {
+        // TODO Auto-generated method stub
+        return box.boxInteger(unbox.unboxInteger(¢) + 1);
+      }
+
+      @Override public boolean check(@Nullable Integer n) {
+        // TODO Auto-generated method stub
+        return false;
+      }
+
+      @Override @Nullable public Integer current() {
+        return null;
+      }
+     
+    }).description().contains("Rule"));
+  }
+}
