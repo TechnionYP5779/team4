8f90880464b6669fca6056c702b47275e7e51e9a
Yuval Ron <yuvalron@hotmail.com>
2018-11-19 12:39:12 +0200

    #52 Improved HasTest

--- src/test/java/fluent/ly/HasTest.java
+++ src/test/java/fluent/ly/HasTest.java
@@ -1,6 +1,8 @@
 package fluent.ly;
 
-import java.util.*;
+import static fluent.ly.azzert.*;
+
+import java.util.*; 
 
 import org.jetbrains.annotations.*;
 import org.junit.*;
@@ -9,7 +11,10 @@ import org.junit.*;
   @Nullable private final String nul = null;
 
   @Test public void seriesA01() {
-    assert has.nulls(nul);
+    azzert.aye(has.nulls(nul));
+    azzert.aye(has.nulls((Object)null));
+    azzert.aye(has.nulls((Object)null, (Object)null, (Object)null));
+    azzert.nay(has.nulls());
   }
 
   @Test public void seriesA02() {
@@ -17,5 +22,26 @@ import org.junit.*;
     @NotNull final Iterable<@Nullable String> os = new ArrayList<>();
     ((List<String>) os).add(String.valueOf("A"));
     azzert.nay(has.nulls(os));
+  }
+  
+  @Test public void seriesA03() {
+    final List<String> i = new ArrayList<>();
+    i.add("");
+    i.add(null);
+    i.add("a");
+    azzert.nay(has.nulls(i));
+  }
+  
+  @Test @SuppressWarnings("null") public void seriesA04() {
+    final List<String> i = new ArrayList<>();
+    azzert.isNull(has.next(-1, i));
+    azzert.isNull(has.next(0, i));
+    azzert.isNull(has.next(1, i));
+    azzert.isNull(has.next(2, i));
+    
+    i.add("");
+    azzert.that(has.next(-1, i), is(""));
+    azzert.that(has.next(0, i), is(""));
+    azzert.that(has.next(1, i), is(""));
   }
 }
