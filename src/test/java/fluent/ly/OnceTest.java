629dd38ba520983a06f0b671a79a0f5bb57f8959
Yuval Ron <yuvalron@hotmail.com>
2018-11-19 13:40:22 +0200

    #52 Added OnceTest

---
+++ src/test/java/fluent/ly/OnceTest.java
@@ -0,0 +1,23 @@
+package fluent.ly;
+
+import static fluent.ly.azzert.*;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class OnceTest {
+  @Test public void case1() {
+    azzert.that(new once("1") + "", is("1"));
+  }
+  
+  @Test public void case2() {
+    azzert.that(new once("2") + "", is("2"));
+  }
+  
+  @Test public void case3() {
+    azzert.that(new once("abc123") + "", is("abc123"));
+  }
+  
+  @Test public void case4() {
+    azzert.that(new once("") + "", is(""));
+  }
+}
