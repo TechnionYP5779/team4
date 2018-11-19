c9d03838ebbd186f7290b2450f0ceb2c0180c904
Yuval Ron <yuvalron@hotmail.com>
2018-11-19 13:13:19 +0200

    #52 Added DeTest

---
+++ src/test/java/fluent/ly/DeTest.java
@@ -0,0 +1,16 @@
+package fluent.ly;
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class DeTest {
+  @Test public void fault1() {
+    azzert.notNull(de.fault("a"));
+  }
+  
+  @Test public void fault2() {
+    azzert.notNull(de.fault(null));
+  }
+  
+  @Test public void fault3() {
+    azzert.notNull(de.fault(Integer.valueOf(1)));
+  }
+}
